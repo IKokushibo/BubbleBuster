6ec33eecb89e2a9f8a4cf521b103dba14cc1f1c7
CJ <x.tiaaantorres@gmail.com>
2023-12-16 22:46:29 +0800

    screen

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((1280, 720))
 
 
-while True:
-    pass
+pygame.display.set_caption("Bubble Buster")
+icon = pygame.image.load('NDTC.png')
+pygame.display.set_icon(icon)
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
